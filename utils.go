ed18600ef25ea676f9f857e26e86f8830c0b0e83
Aksel Arzuman <akselarzuman@gmail.com>
2022-03-06 15:21:30 +0300

    * contains * contains unit tests

--- utils.go
+++ utils.go
@@ -1,5 +1,18 @@
 package utils
 
+// Contains checks whether the value is in slice or not.
+// It supports int, int8, int16, int32, int64, float32, float64, uint, uint8, uint16, uint32, uint64 and string
+// as types for slice values.
+func Contains[T int | int8 | int16 | int32 | int64 | float32 | float64 | uint | uint8 | uint16 | uint32 | uint64 | string](slice []T, value T) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+
+	return false
+}
+
 // Sum sums the values of slice.
 // It supports int, int8, int16, int32, int64, float32, float64, uint, uint8, uint16, uint32 and uint64
 // as types for slice values.
--- utils_test.go
+++ utils_test.go
@@ -1,6 +1,556 @@
 package utils
 
-import "testing"
+import (
+	"testing"
+)
+
+func TestContains(t *testing.T) {
+	t.Run("int scenarios", func(t *testing.T) {
+		t.Run("returns true if element is in slice", func(t *testing.T) {
+			input := []int{1, 2, 3, 4}
+			element := 2
+			result := Contains(input, element)
+
+			if !result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if element is not in slice", func(t *testing.T) {
+			input := []int{1, 2, 3, 4}
+			element := 5
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is empty", func(t *testing.T) {
+			input := []int{}
+			element := 5
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is nil", func(t *testing.T) {
+			input := []int(nil)
+			element := 5
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+	})
+
+	t.Run("int8 scenarios", func(t *testing.T) {
+		t.Run("returns true if element is in slice", func(t *testing.T) {
+			input := []int8{1, 2, 3, 4}
+			element := int8(2)
+			result := Contains(input, element)
+
+			if !result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if element is not in slice", func(t *testing.T) {
+			input := []int8{1, 2, 3, 4}
+			element := int8(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is empty", func(t *testing.T) {
+			input := []int8{}
+			element := int8(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is nil", func(t *testing.T) {
+			input := []int8(nil)
+			element := int8(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+	})
+
+	t.Run("int16 scenarios", func(t *testing.T) {
+		t.Run("returns true if element is in slice", func(t *testing.T) {
+			input := []int16{1, 2, 3, 4}
+			element := int16(2)
+			result := Contains(input, element)
+
+			if !result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if element is not in slice", func(t *testing.T) {
+			input := []int16{1, 2, 3, 4}
+			element := int16(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is empty", func(t *testing.T) {
+			input := []int16{}
+			element := int16(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is nil", func(t *testing.T) {
+			input := []int16(nil)
+			element := int16(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+	})
+
+	t.Run("int32 scenarios", func(t *testing.T) {
+		t.Run("returns true if element is in slice", func(t *testing.T) {
+			input := []int32{1, 2, 3, 4}
+			element := int32(2)
+			result := Contains(input, element)
+
+			if !result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if element is not in slice", func(t *testing.T) {
+			input := []int32{1, 2, 3, 4}
+			element := int32(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is empty", func(t *testing.T) {
+			input := []int32{}
+			element := int32(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is nil", func(t *testing.T) {
+			input := []int32(nil)
+			element := int32(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+	})
+
+	t.Run("int64 scenarios", func(t *testing.T) {
+		t.Run("returns true if element is in slice", func(t *testing.T) {
+			input := []int64{1, 2, 3, 4}
+			element := int64(2)
+			result := Contains(input, element)
+
+			if !result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if element is not in slice", func(t *testing.T) {
+			input := []int64{1, 2, 3, 4}
+			element := int64(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is empty", func(t *testing.T) {
+			input := []int64{}
+			element := int64(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is nil", func(t *testing.T) {
+			input := []int64(nil)
+			element := int64(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+	})
+
+	t.Run("float32 scenarios", func(t *testing.T) {
+		t.Run("returns true if element is in slice", func(t *testing.T) {
+			input := []float32{1, 2, 3, 4}
+			element := float32(2)
+			result := Contains(input, element)
+
+			if !result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if element is not in slice", func(t *testing.T) {
+			input := []float32{1, 2, 3, 4}
+			element := float32(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is empty", func(t *testing.T) {
+			input := []float32{}
+			element := float32(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is nil", func(t *testing.T) {
+			input := []float32(nil)
+			element := float32(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+	})
+
+	t.Run("float64 scenarios", func(t *testing.T) {
+		t.Run("returns true if element is in slice", func(t *testing.T) {
+			input := []float64{1, 2, 3, 4}
+			element := float64(2)
+			result := Contains(input, element)
+
+			if !result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if element is not in slice", func(t *testing.T) {
+			input := []float64{1, 2, 3, 4}
+			element := float64(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is empty", func(t *testing.T) {
+			input := []float64{}
+			element := float64(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is nil", func(t *testing.T) {
+			input := []float64(nil)
+			element := float64(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+	})
+
+	t.Run("uint scenarios", func(t *testing.T) {
+		t.Run("returns true if element is in slice", func(t *testing.T) {
+			input := []uint{1, 2, 3, 4}
+			element := uint(2)
+			result := Contains(input, element)
+
+			if !result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if element is not in slice", func(t *testing.T) {
+			input := []uint{1, 2, 3, 4}
+			element := uint(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is empty", func(t *testing.T) {
+			input := []uint{}
+			element := uint(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is nil", func(t *testing.T) {
+			input := []uint(nil)
+			element := uint(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+	})
+
+	t.Run("uint8 scenarios", func(t *testing.T) {
+		t.Run("returns true if element is in slice", func(t *testing.T) {
+			input := []uint8{1, 2, 3, 4}
+			element := uint8(2)
+			result := Contains(input, element)
+
+			if !result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if element is not in slice", func(t *testing.T) {
+			input := []uint8{1, 2, 3, 4}
+			element := uint8(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is empty", func(t *testing.T) {
+			input := []uint8{}
+			element := uint8(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is nil", func(t *testing.T) {
+			input := []uint8(nil)
+			element := uint8(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+	})
+
+	t.Run("uint16 scenarios", func(t *testing.T) {
+		t.Run("returns true if element is in slice", func(t *testing.T) {
+			input := []uint16{1, 2, 3, 4}
+			element := uint16(2)
+			result := Contains(input, element)
+
+			if !result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if element is not in slice", func(t *testing.T) {
+			input := []uint16{1, 2, 3, 4}
+			element := uint16(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is empty", func(t *testing.T) {
+			input := []uint16{}
+			element := uint16(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is nil", func(t *testing.T) {
+			input := []uint16(nil)
+			element := uint16(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+	})
+
+	t.Run("uint32 scenarios", func(t *testing.T) {
+		t.Run("returns true if element is in slice", func(t *testing.T) {
+			input := []uint32{1, 2, 3, 4}
+			element := uint32(2)
+			result := Contains(input, element)
+
+			if !result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if element is not in slice", func(t *testing.T) {
+			input := []uint32{1, 2, 3, 4}
+			element := uint32(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is empty", func(t *testing.T) {
+			input := []uint32{}
+			element := uint32(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is nil", func(t *testing.T) {
+			input := []uint32(nil)
+			element := uint32(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+	})
+
+	t.Run("uint64 scenarios", func(t *testing.T) {
+		t.Run("returns true if element is in slice", func(t *testing.T) {
+			input := []uint64{1, 2, 3, 4}
+			element := uint64(2)
+			result := Contains(input, element)
+
+			if !result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if element is not in slice", func(t *testing.T) {
+			input := []uint64{1, 2, 3, 4}
+			element := uint64(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is empty", func(t *testing.T) {
+			input := []uint64{}
+			element := uint64(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is nil", func(t *testing.T) {
+			input := []uint64(nil)
+			element := uint64(5)
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+	})
+
+	t.Run("string scenarios", func(t *testing.T) {
+		t.Run("returns true if element is in slice", func(t *testing.T) {
+			input := []string{"1", "2", "3", "4"}
+			element := "2"
+			result := Contains(input, element)
+
+			if !result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if element is not in slice", func(t *testing.T) {
+			input := []string{"1", "2", "3", "4"}
+			element := "5"
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is empty", func(t *testing.T) {
+			input := []string{}
+			element := "5"
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+
+		t.Run("returns false if slice is nil", func(t *testing.T) {
+			input := []string(nil)
+			element := "5"
+			result := Contains(input, element)
+
+			if result {
+				t.Errorf("test failed")
+			}
+		})
+	})
+}
 
 func TestSum_Success_Cases(t *testing.T) {
 	result := 10
